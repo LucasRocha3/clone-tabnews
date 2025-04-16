c6a60c3eb672bb35ae3f496ca17d2179251ed7df
LucasRocha3 <125520815+LucasRocha3@users.noreply.github.com>
2025-04-16 16:25:35 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,9 @@
-function status(req, res) {
-  res.status(200).json({
-    status: "ok",
-    message: "API is running",
-  });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  response.status(200).json({ mensagem: "ta dando certo" });
 }
 
 export default status;
